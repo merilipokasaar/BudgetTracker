e1a1bc730b56a246472442a440d8fde42377d000
merilipokasaar <Merilipokasaar@gmail.com>
2024-02-17 12:03:37 +0200

    new

---
+++ src/test/java/BudgetTrackerTest.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class BudgetTrackerTest {
+
+@Test
+    void
+    testStart() {
+
+    BudgetTracker budgetTracker = new BudgetTracker();
+    budgetTracker.start();
+
+   
+    }
+}
+
